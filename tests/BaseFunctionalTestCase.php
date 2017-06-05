ca49efb3ba3c56a11ea502bc68eed14b5e6bf124
Leslie Price <leslie.price78@gmail.com>
2017-06-05 22:06:56 +0200

    no message

--- tests/BaseFunctionalTestCase.php
+++ tests/BaseFunctionalTestCase.php
@@ -20,18 +20,6 @@ abstract class BaseFunctionalTestCase extends Orchestra
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('citrix.auth_type', 'direct');
-
-        /*
-        $app['config']->set('citrix.direct.CITRIX_DIRECT_USER', 'test@test.com');
-        $app['config']->set('citrix.direct.CITRIX_DIRECT_PASSWORD', 'password123');
-        $app['config']->set('citrix.direct.CITRIX_CONSUMER_KEY', 'abcdefg');
-
-        $app['config']->set('citrix.oauth2.CITRIX_OAUTH_CONSUMER_KEY', 'direct');
-        $app['config']->set('citrix.oauth2.CITRIX_OAUTH_CONSUMER_SECRET', 'direct');
-        $app['config']->set('citrix.oauth2.CITRIX_OAUTH_REFRESH_KEY', 'direct');
-        $app['config']->set('citrix.oauth2.CITRIX_OAUTH_REDIRECT_URI', 'direct');
-        */
-
         $app['config']->set('mail.driver', 'log');
     }
 
--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -8,25 +8,9 @@ use Slakbal\Citrix\Test\BaseUnitTestCase;
 class ServiceTest extends BaseUnitTestCase
 {
     /** @test */
-    public function it_can_determine_a_human_readable_filesize()
+    public function it_can_show_an_example_test()
     {
         $this->assertEquals('A', 'A');
-        //$this->assertEquals('100 B', Format::humanReadableSize(100));
-        //$this->assertEquals('1000 B', Format::humanReadableSize(1000));
-        //$this->assertEquals('9.77 KB', Format::humanReadableSize(10000));
-        //$this->assertEquals('976.56 KB', Format::humanReadableSize(1000000));
-        //$this->assertEquals('9.54 MB', Format::humanReadableSize(10000000));
-        //$this->assertEquals('9.31 GB', Format::humanReadableSize(10000000000));
     }
 
-    /** @test */
- /*   public function it_can_determine_the_age_in_days()
-    {
-        Carbon::setTestNow(Carbon::create(2016, 1, 1)->startOfDay());
-        $this->assertEquals('0.00 (1 second ago)', Format::ageInDays(Carbon::now()));
-        $this->assertEquals('0.04 (1 hour ago)', Format::ageInDays(Carbon::now()->subHour(1)));
-        $this->assertEquals('1.04 (1 day ago)', Format::ageInDays(Carbon::now()->subHour(1)->subDay(1)));
-        $this->assertEquals('30.04 (4 weeks ago)', Format::ageInDays(Carbon::now()->subHour(1)->subMonths(1)));
-    }
- */
 }
